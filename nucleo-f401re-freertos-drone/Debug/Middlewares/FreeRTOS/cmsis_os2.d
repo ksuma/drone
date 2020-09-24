e026d8c62b760b04c634bf0eee163b9e4761c361
Suma Karol <ksuma@grupainteger.pl>
2020-09-24 19:53:28 +0200

    FreeRTOS configuration

---
+++ nucleo-f401re-freertos-drone/Debug/Middlewares/FreeRTOS/cmsis_os2.d
@@ -0,0 +1,53 @@
+Middlewares/FreeRTOS/cmsis_os2.o: \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+../Core/Inc/FreeRTOSConfig.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
---
+++ nucleo-f401re-freertos-drone/Debug/Middlewares/FreeRTOS/croutine.d
@@ -0,0 +1,32 @@
+Middlewares/FreeRTOS/croutine.o: \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/croutine.c \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/croutine.h
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+../Core/Inc/FreeRTOSConfig.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+C:/Users/KSuma/STM32Cube/Repository/STM32Cube_FW_F4_V1.25.1/Middlewares/Third_Party/FreeRTOS/Source/include/croutine.h:
